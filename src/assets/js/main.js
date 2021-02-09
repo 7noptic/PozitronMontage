35a7719a9062eb6c8da6164c13df95d3439ca938
7noptic <7noptic@gmail.com>
2021-02-09 10:11:41 +0300

    init

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -17,4 +17,63 @@ window.addEventListener('DOMContentLoaded', () => {
         },
 
     });
+    let swiperPartners = new Swiper('.swiper-container-partners', {
+        slidesPerView: 6,
+        spaceBetween: 30,
+        
+        allowSlidePrev: true,
+        allowSlideNext: true,
+        navigation: {
+            nextEl: '.partners__next',
+            prevEl: '.partners__prev',
+        },
+
+    });
+    let swiperArticles = new Swiper('.swiper-container-articles', {
+        slidesPerView: 1,
+        spaceBetween: 50,
+        observer: true,
+        observeParents: true,
+        allowSlidePrev: true,
+        allowSlideNext: true,
+        pagination:{
+            el: '.swiper-pagination-bullets',
+            type: 'bullets',
+            clickable: true,
+        }
+
+    });
+    /* TABS */
+    let articleLink = document.querySelectorAll('.js-article-link'),
+        articleTabs = document.querySelectorAll('.js-article-tabs'),
+        articleParents = document.querySelector('.articles-block');
+
+    if(articleParents){
+        hideTabs(articleLink, articleTabs);
+        showTabs(0, articleLink, articleTabs);
+        articleParents.addEventListener('click', (e) => {
+            if(e.target && e.target.classList.contains('js-article-link')){
+                e.preventDefault();
+                articleLink.forEach((item, i) => {
+                    if(item == e.target){
+                        hideTabs(articleLink, articleTabs);
+                        showTabs(i, articleLink, articleTabs);
+                    }
+                });
+            }        
+        });
+
+    }
+    function showTabs(i = 0,link, content){
+        link[i].classList.add('active');
+        content[i].classList.add('active');
+    }
+    function hideTabs(link, content){
+        link.forEach(item =>{
+            item.classList.remove('active');
+        });
+        content.forEach(item =>{
+            item.classList.remove('active');
+        });
+    }
 });
